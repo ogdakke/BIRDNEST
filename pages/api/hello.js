3f83c7c42d07fa0d3bbab231270ca22a2b5ce41f
ogdakke <daniel.wargh1@gmail.com>
2022-12-21 16:11:56 +0200

    toimii jos selaimessa cors-lisäosa

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,19 @@
+import axios from "axios";
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const url = "http://assignments.reaktor.com/birdnest/drones"
+
+  await axios.get(url, {
+      headers: {
+
+      }
+  })
+  .then(res => {
+      console.log(res);
+  })
+
+
+
+  res.status(200).json(res)
 }
--- pages/drones.js
+++ pages/drones.js
@@ -1,36 +1,65 @@
-export default function Drones (req, res) {
-    fetch('assignments.reaktor.com/birdnest/drones')
-    .then(res => res.text())
-    .then(data => {
-        console.log(data);
-        let parser = new DOMParser()
-        let text = parser.parseFromString(data, 'application/xml')
-        document.createElement('div').textContent = data
-        console.log(text);
-    })
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { xml2json } from 'xml-js'
 
+
+export default function get () {
+    
+    const url = "http://assignments.reaktor.com/birdnest/drones"
+    
+
+    const [data, setData] = useState([])
+    const [isLoading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+        axios.get(url, {})
+            .then(data => {
+                setLoading(false)
+                setData(data)
+                console.log(data)
+            })
+    }, [])
+    
+
+    if (isLoading) return <p>Loading...</p>
+    if (!data) return <p>No data found.</p>
     return (
-        <>
-            <div>
-                <h3>Drones in area</h3>
-                <ul>
-                    
-                </ul>
-            </div>
-        </>
+        <p>ready</p>
     )
 }
+// export default function Drones () {
+//     const url = "https://assignments.reaktor.com/birdnest/drones"
+    
+    
+
+//     return (
+//         <>
+//             <div>
+//                 <h3>Drones in area</h3>
+//                 <ul>
+//                     <p>
+                        
+//                     </p>
+//                 </ul>
+//             </div>
+//         </>
+//     )
+// }
 
 
-// export async function getStaticProps() {
-//     const xml = await fetch('https://assignments.reaktor.com/birdnest/drones')
-//     .then(res => res.text())
-//     .then(data => {
-//         console.log(data);
-//         let parser = new DOMParser()
-//         let text = parser.parseFromString(data, 'application/xml')
-//     })
-//     return {
-//        props: {text}
+// export async function get(res) {
+//     try {
+//         await fetch('assignments.reaktor.com/birdnest/drones')
+//         .then(res => res.text())
+//         .then(data => {
+//             let parser = new DOMParser()
+//             let text = JSON.parse(parser.parseFromString(data, 'application/xml'))
+//             console.log(text);
+//             console.log(data);
+//         })
+        
+//     } catch (error) {
+        
 //     }
 // }
